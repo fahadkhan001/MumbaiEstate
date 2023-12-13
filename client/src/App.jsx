5816d53415164acd071760d846faf356499ce60f
fahad khan <kkfahad1530@gmail.com>
2023-12-13 20:12:01 +0530

    update header and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Header from './components/Header';
+import PrivateRoutes from './components/PrivateRoutes';
 
 
 export default function App () {
@@ -16,7 +17,9 @@ export default function App () {
   <Route path='/sign-in' element={<Signin />} />
   <Route path='/sign-up' element={<SignUp />} />
   <Route path='/about' element={<About />} />
+  <Route element={<PrivateRoutes />} >
   <Route path='/profile' element={<Profile />} />
+  </Route>
   </Routes>
   </BrowserRouter>
   )
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -66,7 +66,7 @@ export default function SignIn() {
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
-        <PhoneNumber/>
+       {/* <PhoneNumber/> */}
         <OAuth/>
         
       </form>
