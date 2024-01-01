a5cbc8c68a43e17572e9b9579711345b2fbde02a
fahad khan <kkfahad1530@gmail.com>
2024-01-01 20:02:23 +0530

    complete header search form funcitonality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,35 @@
 import {FaSearch} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { useSelector } from 'react-redux'
+import { useEffect, useState } from 'react'
 
 export default function Header() {
   const {currentUser}= useSelector(state=>state.user)
+  const [searchTerm,setSearchTerm] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+  //react has a built in javascript constructor for the string we need to add after search term
+  const urlParams =  new URLSearchParams(window.location.search);
+  //when we have all the terms we need to change its search term
+  urlParams.set('searchTerm',searchTerm); 
+  //we need to convert the urlparam to string coz we can have number s also
+  const searchQuery = urlParams.toString();
+  navigate(`/search?${searchQuery}`);
+
+  }
+
+  // we use useeffect so that eevrytome we change our serach term it hcanges
+  useEffect(()=>{
+    const urlParams = new URLSearchParams(window.location.search)
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if(searchTermFromUrl){
+      setSearchTerm(searchTermFromUrl)
+    }
+  },[location.search])
+
+
   return (
     <header className='bg-blue-700 shadow-md '>
     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -15,10 +41,14 @@ export default function Header() {
     </Link>
     {/*Search bar */}
 
-    <form className='bg-blue-600 p-3 rounded-lg flex items-center'>
-    <input type='text'  placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/> {/*to remove the outline we need to use focus:outlinenone */}
+    <form onSubmit={handleSubmit} className='bg-blue-600 p-3 rounded-lg flex items-center'>
+    <input type='text' value={searchTerm} onChange={(e)=>setSearchTerm(e.target.value)}  placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64'/> {/*to remove the outline we need to use focus:outlinenone */}
+    <button>
     <FaSearch className='text-blue-400 '/>
+    </button>
     </form>
+
+
     <ul className='flex gap-4 '>
     <Link to="/">
     <li className='hidden sm:inline text-gray-50 hover:bg-emerald-800' >Home</li>
