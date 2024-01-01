898c7cf287565ea9768f26f0bbfe53de0e4de00d
fahad khan <kkfahad1530@gmail.com>
2024-01-01 19:13:35 +0530

    create search api route

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -1,6 +1,7 @@
 import { AxiosError } from "axios";
 import Listing from "../models/listingModel.js";
 import { errorHandler } from "../utils/error.js";
+import { request } from "express";
 
 export const createListing=async(req,res,next)=>{
     try {
@@ -67,3 +68,57 @@ export const updateListing = async (req, res, next) => {
     }
   }
 
+export const getListings = async(req,res,next)=>{
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    //if there is not start index we use 0
+    const startIndex = parseInt(req.query.startIndex) || 0;
+     let offer = req.query.offer;
+    //the offer can be true or false or undefined ans in starting we need it to be falsse
+    if(offer === 'false' || offer ===undefined ){
+      offer={$in:[false,true] };
+    }
+
+    let furnished = req.query.furnished;
+    if(furnished==='false' || furnished===undefined)
+    {
+      furnished = {$in:[false,true]};
+    }
+    let parking = req.query.parking;
+    if(parking==='false' || parking===undefined)
+    {
+      parking = {$in:[false,true]};
+    }
+
+    let type  =req.query.type ;
+    if(type==='all'|| type===undefined){
+      type= {$in:['sale','rent']};
+    } 
+
+    const searchTerm = req.query.searchTerm || '';
+
+    const sort = req.query.sort || 'createdAt';
+
+    const order = req.query.order || 'desc';
+
+    const listings =  await Listing.find({
+      //regex is like i f you have some tittle it will search for the term it is built in search functionality in monogDB
+      //${option :i is for not diffrentitating bitween upper case and lower case}
+      name: { $regex:searchTerm,  $options:'i'},
+      offer,
+      type,
+      furnished,
+      parking
+    }).sort(
+      {[sort]:order}
+    ).limit(limit).skip(startIndex);
+
+
+    return res.status(200).json(listings)
+
+
+
+  } catch (error) {
+    next(error);
+  }
+}
