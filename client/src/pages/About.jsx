6d06bd8b57293b371b8c1084f56d647881340c75
fahad khan <kkfahad1530@gmail.com>
2024-01-05 19:56:21 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 mx-auto max-w-6xl'>
+    <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Mumbai Estate</h1>
+    <p className='mb-4 text-slate-700'>
+    Mumbai Estate, your premier real estate companion, redefines the art of home discovery. With an intuitive and user-friendly app, we bring the city's finest properties to your fingertips. Navigate effortlessly through our curated listings, each representing a unique blend of comfort, style, and modern living. Mumbai Estate is more than an application; it's your personalized guide to unlocking the door to dream homes. Embrace a seamless, enjoyable, and efficient property search with us as you embark on the journey to find the perfect address for your aspirations.
+    </p>
+    <p className='mb-4 text-slate-700'>
+    Mumbai Estate is the brainchild of Mohammed Fahad Khan, a MERN stack developer with a passion for creating innovative and user-centric solutions. With a keen eye for detail and a commitment to excellence, Fahad has crafted an application that seamlessly combines technology with the intricacies of the real estate world. As the driving force behind Mumbai Estate, he envisions a platform that not only simplifies the property search process but also elevates the entire experience for users. With a foundation built on robust technology and a dedication to user satisfaction, Mohammed Fahad Khan brings his expertise to the forefront, ensuring Mumbai Estate stands as a testament to modern, efficient, and enjoyable home discovery
+    </p>
+    <p className='mb-4 text-slate-800 '>
+    for more information contact <span className='hover:underline'>kkfahad1530@gmail.com</span>
+    </p>
+    </div>
   )
 }
 
